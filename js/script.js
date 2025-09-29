98c56d1ad8eba68890cb4a0527a771a3041b4752
Alexis Soratti <115167272+Godalexis@users.noreply.github.com>
2025-09-29 15:48:24 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,107 @@
+let loadMoreBtn = document.querySelector('#load-more');
+let currentItem = 8;
+
+loadMoreBtn.onclick = () => {
+    let boxes = [...document.querySelectorAll('.box-container .box')];
+    for (let i = currentItem; i < currentItem + 4; i++) {
+        boxes[i].style.display = 'inline-block';
+    }
+    currentItem += 4;
+
+    if (currentItem >= boxes.length) {
+        loadMoreBtn.style.display = 'none';
+    }
+}
+
+const carrito = document.getElementById('carrito');
+const elementos1 = document.getElementById('lista-1');
+const lista = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
+
+cargarEventListeners();
+
+function cargarEventListeners() {
+    elementos1.addEventListener('click', comprarElemento);
+    carrito.addEventListener('click', eliminarElemento);
+
+    vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+}
+
+function comprarElemento(e) {
+    e.preventDefault();
+    if(e.target.classList.contains('agregar-carrito')) {
+        const elemento = e.target.parentElement.parentElement;
+        leerDatosElemento(elemento);
+    }
+}
+
+function leerDatosElemento(elemento) {
+    const infoElemento = {
+        imagen: elemento.querySelector('img').src,
+        titulo: elemento.querySelector('h3').textContent,
+        precio: elemento.querySelector('.precio').textContent,
+        id: elemento.querySelector('a').getAttribute('data-id')
+    }
+
+    insertarCarrito(infoElemento);
+}
+
+function insertarCarrito(elemento) {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+        <td>
+            <img src="${elemento.imagen}" width=100 height=150px >
+        </td>
+        <td>
+            ${elemento.titulo}
+        </td>
+        <td>
+            ${elemento.precio}
+        </td>
+        <td>
+            <a herf="#" class="borrar" data-id="${elemento.id}" >X</a>
+        </td>
+    `;
+    lista.appendChild(row);
+}
+
+function eliminarElemento(e){
+    e.preventDefault();
+    let elemento, 
+        elementoId;
+
+    if(e.target.classList.contains('borrar')) {
+        e.target.parentElement.parentElement.remove();
+        elemento = e.target.parentElement.parentElement;
+        elementoId = elemento.querySelector('a').getAttribute('data-id');
+    }
+}
+
+function vaciarCarrito(e) {
+    while(lista.firstChild) {
+        lista.removeChild(lista.firstChild);
+    }
+    return false;
+}
+
+const slider = document.querySelector('.slider-container');
+const slides = document.querySelectorAll('.slide');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+let index = 0;
+
+function showSlide(n) {
+    if (n < 0) index = slides.length - 1;
+    else if (n >= slides.length) index = 0;
+    else index = n;
+
+    slider.style.transform = `translateX(${-index * 100}%)`;
+}
+
+prev.addEventListener('click', () => showSlide(index - 1));
+next.addEventListener('click', () => showSlide(index + 1));
+
+setInterval(() => {
+    showSlide(index + 1);
+}, 3000);
